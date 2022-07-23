c3d8114151c887bdfeb6f6d3afd7a2c8865128a9
Ben Menking <bmenking@wng.org>
2022-07-23 01:05:56 -0400

    fixes breaking change introduced in previous commit

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -21,8 +21,8 @@ class Model {
      * @internal
      */
     public function __get($name) {
-        if( in_array($name, static::$fields) && isset(static::$fields[$name]) ) {
-            return $this->values['result'][$name];
+        if( in_array($name, static::$fields) && isset($this->values[$name]) ) {
+            return $this->values[$name];
         }
     }
 
@@ -30,6 +30,6 @@ class Model {
      * @internal
      */
     public function __isset($name) {
-        return (in_array($name, static::$fields) && isset(static::$fields[$name]));
+        return (in_array($name, static::$fields) && isset($this->values[$name]));
     }
 }
